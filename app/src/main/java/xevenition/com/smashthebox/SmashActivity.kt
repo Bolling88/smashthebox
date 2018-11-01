5a21354856fb000f5e4b3899e218e2d3755c1d18
Ludwig <bolling.ludwig@gmail.com>
2018-11-01 22:48:31 +0100

    More fixes with setting up the ball

--- app/src/main/java/xevenition/com/smashthebox/SmashActivity.kt
+++ app/src/main/java/xevenition/com/smashthebox/SmashActivity.kt
@@ -1,8 +1,12 @@
 package xevenition.com.smashthebox
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -14,15 +18,14 @@ class SmashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySmashBinding
 
+    private lateinit var ball: Ball
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(
-            this, R.layout.activity_smash)
-
-        val ball = Ball(this)
-
+            this, R.layout.activity_smash
+        )
         val model = ViewModelProviders.of(this).get(SmashViewModel::class.java)
-        model.setBall(ball)
         binding.viewModel = model
         model.observableSmashForce.observe(this, Observer {
             it?.let {
@@ -31,5 +34,104 @@ class SmashActivity : AppCompatActivity() {
                 binding.imageView.startAnimation(myAnim)
             }
         })
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                MY_PERMISSIONS_REQUEST_BLE
+            )
+        } else {
+            // Permission has already been granted
+            ball = Ball(this)
+            handleBallConnectionState(ball)
+            handleBallBounce(ball)
+        }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_BLE -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+        }
+    }
+
+    private fun handleBallBounce(ball: Ball) {
+        ball.setOnBounceListener { bounceType, totalForce ->
+            when (bounceType) {
+                Ball.BounceType.HARD -> {
+
+                }
+                Ball.BounceType.NORMAL -> {
+
+                }
+                Ball.BounceType.SOFT -> {
+
+                }
+            }
+        }
+    }
+
+    private fun handleBallConnectionState(ball: Ball) {
+        ball.setOnConnectionStateChangedListener { connectionState, stateMessage ->
+            when (connectionState) {
+                Ball.ConnectionState.CONNECTING -> {
+
+                }
+                Ball.ConnectionState.BLE_SERVICE_READY -> {
+
+                }
+                Ball.ConnectionState.BLE_SERVICE_STARTING -> {
+
+                }
+                Ball.ConnectionState.CONNECTED -> {
+
+                }
+                Ball.ConnectionState.CONNECTION_FAILED -> {
+
+                }
+                Ball.ConnectionState.DISCONNECTED -> {
+
+                }
+                Ball.ConnectionState.DISCONNECTING -> {
+
+                }
+                Ball.ConnectionState.ERROR -> {
+
+                }
+                Ball.ConnectionState.SCANNING_ABORTED -> {
+
+                }
+                Ball.ConnectionState.SCANNING_ENDED -> {
+
+                }
+                Ball.ConnectionState.BLE_SERVICE_STARTING -> {
+
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+//        ball.disconnectDevice()
+//        ball.onDestroy()
+    }
+
+    companion object {
+        const val MY_PERMISSIONS_REQUEST_BLE = 34231
+    }
+
 }
--- app/src/main/java/xevenition/com/smashthebox/SmashViewModel.kt
+++ app/src/main/java/xevenition/com/smashthebox/SmashViewModel.kt
@@ -1,71 +1,14 @@
 package xevenition.com.smashthebox
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.wrlds.sdk.Ball
 
 class SmashViewModel : ViewModel() {
-    var observableSmashForce = MutableLiveData<Float>()
+    val observableSmashForce = SingleLiveEvent<Float>()
+    val observableToast = SingleLiveEvent<String>()
 
 
     fun onBoxClicked() {
         //TODO hard coded a force on the box for testing
         observableSmashForce.postValue(100f)
-    }
-
-    fun setBall(ball: Ball) {
-        ball.setOnConnectionStateChangedListener { connectionState, stateMessage ->
-            when (connectionState) {
-                Ball.ConnectionState.CONNECTING -> {
-
-                }
-                Ball.ConnectionState.BLE_SERVICE_READY -> {
-
-                }
-                Ball.ConnectionState.BLE_SERVICE_STARTING -> {
-
-                }
-                Ball.ConnectionState.CONNECTED -> {
-
-                }
-                Ball.ConnectionState.CONNECTION_FAILED -> {
-
-                }
-                Ball.ConnectionState.DISCONNECTED -> {
-
-                }
-                Ball.ConnectionState.DISCONNECTING -> {
-
-                }
-                Ball.ConnectionState.ERROR ->{
-
-                }
-                Ball.ConnectionState.SCANNING_ABORTED ->{
-
-                }
-                Ball.ConnectionState.SCANNING_ENDED ->{
-
-                }
-                Ball.ConnectionState.BLE_SERVICE_STARTING ->{
-
-                }
-            }
-        }
-
-
-        ball.setOnBounceListener { bounceType, totalForce ->
-            observableSmashForce.postValue(totalForce)
-            when (bounceType) {
-                Ball.BounceType.HARD -> {
-
-                }
-                Ball.BounceType.NORMAL -> {
-
-                }
-                Ball.BounceType.SOFT -> {
-
-                }
-            }
-        }
     }
 }
